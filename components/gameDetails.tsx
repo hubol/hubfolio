30ae16057570165f0c3d8565d6c831f3a1aaadca
Hubol <hubol.gordon@gmail.com>
2020-12-01 00:47:12 -0600

    More wack style for details

--- components/gameDetails.tsx
+++ components/gameDetails.tsx
@@ -5,8 +5,34 @@ export function GameDetails({ game }: {game: Game}) {
     return <>
         <section dangerouslySetInnerHTML={{__html: game.detailsHtml}}/>
         <style jsx>{`
+section {
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 0.5em;
+}
+
 :global(p) {
     color: green;
+    width: 100%;
+}
+
+:global(blockquote) {
+  width: 90%;
+  font-style: italic;
+}
+
+:global(blockquote), :global(p) {
+  margin-bottom: 0.5em;
+}
+
+:global(iframe) {
+  margin: 0.5em 0;
+}
+
+:global(iframe[src*="bandcamp"]) {
+  max-width: 700px;
 }`}</style>
     </>
 }
