1bc8464edc8a40df50c4c5d4ce77eb4f85da8a49
Hubol <hubol.gordon@gmail.com>
2020-12-11 23:40:34 -0600

    Remove a lot of stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,13 @@
 import Head from 'next/head'
 import * as React from "react";
-import {HubolHeader} from "../components/hubolHeader";
-import {Game, getGamesCatalog} from "../cms/getGamesCatalog";
 
-interface HomeProps {
-    catalog: Game[]
-}
-
-export default function Home({catalog}: HomeProps) {
+export default function Home() {
     return (
         <>
             <Head>
                 <title>Hubolhubolhubol</title>
                 <meta name="description" content="Video games, music, and more by Hubol Persson-Gordon."/>
             </Head>
-            <HubolHeader catalog={catalog}/>
         </>
     )
-}
-
-export async function getStaticProps() {
-    return {props: {catalog: await getGamesCatalog()}};
 }
