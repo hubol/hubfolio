48a80bb9b2c1141cc43115c6a5851e7df843285b
Hubol <hubol.gordon@gmail.com>
2020-12-13 22:44:14 -0600

    Tweak footer, new favicon

--- components/HubolFooter.tsx
+++ components/HubolFooter.tsx
@@ -3,13 +3,10 @@ import * as React from "react";
 export function HubolFooter()
 {
     return <footer>
-        <p>Hello</p>
         <style jsx>{`
 footer {
   background-color: #1A2D9B;
-  color: yellow;
-  padding-top: 32px;
-  min-height: 6em;
+  min-height: 16vmin;
 }`}</style>
     </footer>
 }
